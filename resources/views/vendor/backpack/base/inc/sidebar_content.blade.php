1acffb9f0bf34ecd8fff52b5b7d06da6f2db7bc2
Abdo Salmon <abdo@bodecanada.com>
2021-03-29 19:30:18 +0200

    - Update sidebar

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,12 +1,12 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('user') }}'><i class='nav-icon la la-user'></i> Users</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('userprofile') }}'><i class='nav-icon la la-question'></i> User Profiles</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('category') }}'><i class='nav-icon la la-list'></i> Categories</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('brand') }}'><i class='nav-icon la la-apple-alt'></i> Brands</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('color') }}'><i class='nav-icon la la-quidditch'></i> Colors</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('country') }}'><i class='nav-icon la la-globe-africa'></i> Countries</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('gift') }}'><i class='nav-icon la la-gift'></i> Gifts</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('userprofile') }}'><i class='nav-icon la la-question'></i>User Profiles</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('category') }}'><i class='nav-icon la la-list'></i>Categories</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('brand') }}'><i class='nav-icon la la-apple-alt'></i>Closet brand</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('color') }}'><i class='nav-icon la la-quidditch'></i>Colors</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('country') }}'><i class='nav-icon la la-globe-africa'></i>Countries</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('gift') }}'><i class='nav-icon la la-gift'></i>Fashion store</a></li>
 
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('blog') }}'><i class='nav-icon la la-blog'></i> Blogs</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('blogcomment') }}'><i class='nav-icon la la-commenting-o'></i> Blog Comments</a></li>
