d4ad8ebcc239d97611b6c91041abd1ef333fef5c
Abdo Salmon <abdo@bodecanada.com>
2021-04-13 04:27:55 +0200

    Make subscritpion email unique

--- app/Http/Requests/SubscriptionRequest.php
+++ app/Http/Requests/SubscriptionRequest.php
@@ -26,7 +26,7 @@ class SubscriptionRequest extends FormRequest
     public function rules()
     {
         return [
-          'email' => 'required|email'
+          'email' => 'required|email|unique:subscriptions'
         ];
     }
 
